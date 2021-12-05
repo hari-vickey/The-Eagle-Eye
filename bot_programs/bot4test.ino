4a49b529fc68ad21ee603442934edd13e974fb5d
Bijoy Antony <63728655+Bijoy28@users.noreply.github.com>
2021-12-05 09:51:31 +0530

    Update bot4test.ino

--- bot_programs/bot4test.ino
+++ bot_programs/bot4test.ino
@@ -1,4 +1,4 @@
-// Bot 4 Esp Program to Control the movement of the bot and the Servo Motor
+// Bot 2 Esp Program to Control the movement of the bot and the Servo Motor
 // Important Note : Esp8266 is by default active low state 
 // It means the HIGH State = 0 and LOW  State = 1
 
@@ -23,105 +23,72 @@ int enb = D8;
 int sm = D6;
 
 // Declare Speed Control Values
-int linear = 500;
-int turn = 400;
-
+int SPD = 500;
+int SPD1 = 500;
+int forw = 550;
 // Declare Variable to Store the Value of MPU 6050
 float z = 0;
 float z_ang = 0;
 float z_cal = 0;
-float dev_1 = 0;
-float dev_2 = 0;
-int c=0;
+float zg = 0;
 
 // Function to get the angle from MPU Sensor
-float mpu(bool calibrate=false) {
+float mpu() {
     mpu6050.update();
-    // Getting Calibration Value on setup
-    if (calibrate==true) {
-        z = mpu6050.getAngleZ();
-        Serial.println("MPU Calibration Done");
-        Serial.print("Calibration Value : ");
-        Serial.println(z);
-        //delay(1000);
-    }
-    // Get Yaw Values from MPU
-    else {
-        z = mpu6050.getAngleZ();
-        Serial.print("angleZ : ");
-        Serial.println(z);
-    }
+    z = mpu6050.getAngleZ();
+    // Serial.print("angleZ : ");
+    // Serial.println(z);
     return z;
 }
 
 // Function to Move the bot in different Directions
 void movement(int direction, float angle=0) {
     if (direction == 0) {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
         analogWrite(ena, 0);
         analogWrite(enb, 0);
         Serial.println("Stop");
     }
     if (direction == 1) {
-      c=c+1;
-      z_ang = mpu();
-      if(c<=1)
+        zg = mpu();
+      for(int i=0;i<100;i++)
       {
-        dev_1 = z_ang + 10;
-        dev_2 = z_ang - 10;
-      }
-      if((z_ang <= dev_1) || (z_ang >= dev_2)){
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      digitalWrite(in3, HIGH);
-      digitalWrite(in4, LOW);
-      analogWrite(ena, linear);
-      analogWrite(enb, linear);
-      Serial.println("Forward");
-      }
-      if(z_ang > dev_1){
-          digitalWrite(in1, LOW);
-          digitalWrite(in2, HIGH);
-          digitalWrite(in3, LOW);
-          digitalWrite(in4, HIGH);
-          analogWrite(ena, turn);
-          analogWrite(enb, turn);
-          Serial.println("Right");
-      }
-      if(z_ang < dev_2){
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        analogWrite(ena, turn);
-        analogWrite(enb, turn);
-        Serial.println("Left");
-        }
-      
+        z_ang = mpu();
         digitalWrite(in1, LOW);
         digitalWrite(in2, HIGH);
         digitalWrite(in3, HIGH);
         digitalWrite(in4, LOW);
-        analogWrite(ena, 0);
-        analogWrite(enb, 0);
-        Serial.println("Stop");
+        if(z_ang==zg)
+        {
+          analogWrite(ena, SPD);
+          analogWrite(enb, SPD1);
+        }
+        if(z_ang<zg)
+        {
+          analogWrite(ena, SPD);
+          analogWrite(enb, forw);
+        }
+        if(z_ang>zg)
+        {
+          analogWrite(ena, forw);
+          analogWrite(enb, SPD1);
+        }
+        zg = z_ang;
+        Serial.println("Forward");
+      }
     }
     if (direction == 2) {
         z_ang = mpu();
         z_cal = (-(angle)+z_ang);
         while(z_ang >= z_cal) {
             z_ang = mpu();
-            Serial.print("Z angle : ");
-            Serial.println(z_ang);
+            // Serial.print("Z angle : ");
+            // Serial.println(z_ang);
             digitalWrite(in1, LOW);
             digitalWrite(in2, HIGH);
             digitalWrite(in3, LOW);
             digitalWrite(in4, HIGH);
-            analogWrite(ena, turn);
-            analogWrite(enb, turn);
+            analogWrite(ena, SPD);
+            analogWrite(enb, SPD);
             Serial.println("Right");
         }
         analogWrite(ena, 0);
@@ -137,8 +104,8 @@ void movement(int direction, float angle=0) {
             digitalWrite(in2, LOW);
             digitalWrite(in3, HIGH);
             digitalWrite(in4, LOW);
-            analogWrite(ena, turn);
-            analogWrite(enb, turn);
+            analogWrite(ena, SPD);
+            analogWrite(enb, SPD);
             Serial.println("Left");
         }
         analogWrite(ena, 0);
@@ -146,54 +113,32 @@ void movement(int direction, float angle=0) {
         Serial.println("Stop");
     }
     if (direction == 4) {
+        zg = mpu();
+      for(int i=0;i<100;i++)
+      {
+        z_ang = mpu();
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
         digitalWrite(in3, LOW);
         digitalWrite(in4, HIGH);
-        analogWrite(ena, linear);
-        analogWrite(enb, linear);
+        if(z_ang==zg)
+        {
+          analogWrite(ena, SPD);
+          analogWrite(enb, SPD1);
+        }
+        if(z_ang>zg)
+        {
+          analogWrite(ena, SPD);
+          analogWrite(enb, forw);
+        }
+        if(z_ang<zg)
+        {
+          analogWrite(ena, forw);
+          analogWrite(enb, SPD1);
+        }
+      }
+        zg = z_ang;
         Serial.println("Backward");
-        delay(1000);
-    }
-    if (direction == 5) {
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        analogWrite(ena, turn);
-        analogWrite(enb, turn);
-        Serial.println("Fine Right");
-        delay(100);
-        analogWrite(ena, 0);
-        analogWrite(enb, 0);
-        delay(500);
-    }
-    if (direction == 6) {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        analogWrite(ena, turn);
-        analogWrite(enb, turn);
-        Serial.println("Fine Left");
-        delay(100);
-        analogWrite(ena, 0);
-        analogWrite(enb, 0);
-        delay(500);
-    }
-    if (direction == 7) {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        analogWrite(ena, turn);
-        analogWrite(enb, turn);
-        Serial.println("180 - degree Turn");
-        delay(730);
-        analogWrite(ena, 0);
-        analogWrite(enb, 0);
-        Serial.println("Stop");
-        delay(500);
     }
 }
 
@@ -227,45 +172,31 @@ void setup() {
     Wire.begin();
     // Initialize the MPU 6050 Sensor
     mpu6050.begin();
-    z_cal  = mpu(true);
+    z_cal  = mpu();
+
     movement(1);
     movement(1);
     movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
-    movement(1);
+    movement(4);
+    movement(4);
+    movement(4);
+    movement(2, 45);
     movement(0);
+    delay(1000);
+    servo_control(1);
+    movement(3, 45);
+    movement(0);
+    delay(1000);
+    movement(2, 90);
+    movement(0);
+    delay(1000);
+    servo_control(1);
+    movement(3, 90);
+    movement(0);
+    delay(1000);
 }
 
 // Loop the neccessary Functions
 void loop() {
-    //servo_control(0);
-//    movement(1);
-//    delay(1000);
-//    movement(0);
-//    delay(1000);
-//    movement(2, 45);
-//    delay(1000);
-//    movement(3, 45);
-//    delay(1000);
-//    movement(2, 90);
-//    delay(1000);
-//    movement(3, 90);
-//    delay(1000);
-//    movement(2, 180);
-//    delay(1000);
-//    movement(3, 180);
-//    delay(1000);
-//    movement(1);
-//    delay(2000);
-//    movement(0);
-//    delay(1000);
+    
 }
